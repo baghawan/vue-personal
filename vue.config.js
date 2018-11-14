fa8bb9687b1fdc8f5b8651678083675ded36abac
Dian Baghawan <bp@mineral.co.id>
2018-11-14 12:30:21 +0700

    latest

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,11 @@ module.exports = {
     pluginOptions: {
         'style-resources-loader': {
             preProcessor: 'scss',
-            patterns: [path.resolve(__dirname, "./src/assets/styles/global.scss")]
+            patterns: [
+            	// path.resolve(__dirname, "./src/assets/styles/setup/*.scss"),
+            	// path.resolve(__dirname, "./src/assets/styles/utilities/*.scss"),
+            	path.resolve(__dirname, "./src/assets/styles/global.scss")
+            ]
         }
     }
 }
