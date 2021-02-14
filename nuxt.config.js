c6e3a1ca0ebf951d665fab95d9ad0f465a52b386
Dian Baghawan <dian.baghawan@wundermanthompson.com>
2021-02-14 23:15:45 +0700

    test travis build

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,9 @@
 export default {
+  mode: "universal",
+  target: 'static',
+  router: {
+    base: '/baghawan.github.io/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'baghawan',
@@ -38,10 +43,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  },
-
-  target: 'static',
-  router: {
-    base: '/baghawan.github.io/'
   }
 }
