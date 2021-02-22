f666119fa11fc2871d674536fa9936583923d1fe
Dian Baghawan <dian.baghawan@wundermanthompson.com>
2021-02-22 21:59:47 +0700

    install @nuxt/content

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,48 @@
 export default {
-  mode: "universal",
-  target: 'static',
-  router: {
-    base: '/baghawan.github.io/'
-  },
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'baghawan',
-    htmlAttrs: {
-      lang: 'en'
+    mode: "universal",
+    target: 'static',
+    router: {
+        base: '/baghawan.github.io/'
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'baghawan',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
+    
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+    
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
+    ],
+    
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        '@nuxt/content'
+    ],
+    
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    }
 }
