fe38d962f353d6dc7eaf825ae08451d3d157a50c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-02 11:21:53 +0000

    Deploy baghawan/baghawan.github.io to github.com/baghawan/baghawan.github.io.git:gh-pages

---
+++ _nuxt/static/1614684099/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fworks\u002Fonerdm","\u002Fworks\u002Fpipiltin-cocoa","\u002Fworks\u002Fstudio1212","\u002Fworks\u002Fw-steakngrill","\u002Fworks\u002Fheavenlights"]})
---
+++ _nuxt/static/1614684099/works/w-steakngrill/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fbaghawan.github.io\u002F_nuxt\u002Fstatic\u002F1614684099",layout:"default",error:null,serverRendered:a,routePath:"\u002Fworks\u002Fw-steakngrill",config:{content:{dbHash:"15fbc27d"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
